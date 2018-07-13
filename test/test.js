99b43cdd2749229364464ef98b496f58a6a4edc4
王含泽 <whzcorcd@gmail.com>
2018-07-14 05:06:33 +0800

    更新 Travis CI 配置

--- test/test.js
+++ test/test.js
@@ -12,15 +12,19 @@ app.get('/test', (req, res) => {
 });
 /* ------------------------------------------------------------------------------------------------- */
 
-request(app)
-	.get('/test')
-	.set('Accept', 'application/json')
-	.expect('Content-Type', /json/)
-	.expect(200)
-	.end((err, res) => {
-		if (err) {
-			done(err);
-		}
-		res.body.context.should.be.eql('json test');
-		done();
+describe('GET /test', () => {
+	it('respond with json', (done) => {
+		request(app)
+			.get('/test')
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(200)
+			.end((err, res) => {
+				if (err) {
+					done(err);
+				}
+				res.body.context.should.be.eql('json test');
+				done();
+			})
 	});
+});
