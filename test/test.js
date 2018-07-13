1e65936a830adf1d0d90bfaf182777550b3bf371
王含泽 <whzcorcd@gmail.com>
2018-07-14 05:01:37 +0800

    更新 CI 测试用例

--- test/test.js
+++ test/test.js
@@ -12,19 +12,15 @@ app.get('/test', (req, res) => {
 });
 /* ------------------------------------------------------------------------------------------------- */
 
-describe('GET /test', () => {
-	it('respond with json', (done) => {
-		request(app)
-			.get('/test')
-			.set('Accept', 'application/json')
-			.expect('Content-Type', /json/)
-			.expect(200)
-			.end((err, res) => {
-				if (err) {
-					done(err);
-				}
-				res.body.context.should.be.eql('json test');
-				done();
-			})
+request(app)
+	.get('/test')
+	.set('Accept', 'application/json')
+	.expect('Content-Type', /json/)
+	.expect(200)
+	.end((err, res) => {
+		if (err) {
+			done(err);
+		}
+		res.body.context.should.be.eql('json test');
+		done();
 	});
-});
