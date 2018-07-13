8b204592bc830c2ba12302aa78e9ccf541e3eb79
王含泽 <whzcorcd@gmail.com>
2018-07-13 14:17:09 +0800

    更新 gitignore

--- src/service.js
+++ src/service.js
@@ -1,14 +1,16 @@
-var http = require("http");
+let http = require("http");
 
-http.createServer(function (request, response) {
+http.createServer((request, response) => {
 
-    // 发送 HTTP 头部 
-    // HTTP 状态值: 200 : OK
-    // 内容类型: text/plain
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+  // 发送 HTTP 头部 
+  // HTTP 状态值: 200 : OK
+  // 内容类型: text/plain
+  response.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
 
-    // 发送响应数据 "Hello World"
-    response.end('Hello World\n');
+  // 发送响应数据
+  response.end('Welcome to Paw-Prints Project Background Server\n');
 }).listen(8888);
 
 // 终端打印如下信息
